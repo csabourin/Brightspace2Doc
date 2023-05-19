3409819d8de2e9e5a7cab99e41eceab3a0d57e95
Christian Sabourin <christian.sabourin@canada.ca>
2023-05-19 10:18:57 -0400

    Update headReplace.js

--- headReplace.js
+++ headReplace.js
@@ -1,2 +1,94 @@
- const headReplace = `<head><title>Brighstpace to Docx</title></head>`
-            module.exports = headReplace;
+const headReplace = `<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=Generator content="Microsoft Word 15 (filtered)">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:Georgia;
+	panose-1:2 4 5 2 5 4 5 2 3 3;}
+@font-face
+	{font-family:"Open Sans";}
+@font-face
+	{font-family:"Segoe UI";
+	panose-1:2 11 5 2 4 2 4 2 2 3;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:8.0pt;
+	margin-left:0in;
+	line-height:107%;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;}
+p.MsoHeader, li.MsoHeader, div.MsoHeader
+	{mso-style-link:"Header Char";
+	margin:0in;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;}
+p.MsoFooter, li.MsoFooter, div.MsoFooter
+	{mso-style-link:"Footer Char";
+	margin:0in;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;}
+span.HeaderChar
+	{mso-style-name:"Header Char";
+	mso-style-link:Header;}
+span.FooterChar
+	{mso-style-name:"Footer Char";
+	mso-style-link:Footer;}
+span.CSPSEFPC-TitlesTitres
+	{mso-style-name:"CSPS\/EFPC - Titles\/Titres";
+	font-family:"Georgia",serif;
+	color:#3F2A56;
+	font-weight:bold;}
+p.CSPSEFPC-ParagraphsParagraphes, li.CSPSEFPC-ParagraphsParagraphes, div.CSPSEFPC-ParagraphsParagraphes
+	{mso-style-name:"CSPS\/EFPC - Paragraphs\/Paragraphes";
+	margin-top:0in;
+	margin-right:0in;
+	margin-bottom:8.0pt;
+	margin-left:0in;
+	line-height:107%;
+	font-size:11.0pt;
+	font-family:"Arial",sans-serif;}
+span.CSPS-Title
+	{mso-style-name:"CSPS - Title";
+	font-family:"Georgia",serif;
+	color:#3F2A56;
+	font-weight:bold;}
+p.CSPS-Paragraph, li.CSPS-Paragraph, div.CSPS-Paragraph
+	{mso-style-name:"CSPS - Paragraph";
+	margin-top:0in;
+	margin-right:0in;
+	margin-bottom:8.0pt;
+	margin-left:0in;
+	line-height:107%;
+	font-size:11.0pt;
+	font-family:"Arial",sans-serif;}
+.MsoChpDefault
+	{font-family:"Calibri",sans-serif;}
+.MsoPapDefault
+	{margin-bottom:8.0pt;
+	line-height:107%;}
+ /* Page Definitions */
+ @page WordSection1
+	{size:8.5in 11.0in;
+	margin:1.0in 1.0in 1.0in 1.0in;}
+div.WordSection1
+	{page:WordSection1;}
+ /* List Definitions */
+ ol
+	{margin-bottom:0in;}
+ul
+	{margin-bottom:0in;}
+-->
+</style>
+
+</head>`;
+module.exports = headReplace;
